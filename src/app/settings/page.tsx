fe39ad39a5629d1e04c4ae7a0e7b70f7784efa53
Hycient195 <onyeukwuhycient@gmail.com>
2024-11-12 15:56:56 +0100

    fix: setting page red background

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,6 +1,6 @@
 export default function Settings() {
   return (
-    <main className="flex h-[100dvh] items-center justify-center p-4 bg-test">
+    <main className="flex h-[100dvh] items-center justify-center p-4">
       <h1 className="text-3xl lg:text-4xl font-medium">Coming soon</h1>
     </main>
   )
