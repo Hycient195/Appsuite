5509b1e60b8ac53e4defb76515872ed8f94f2b31
Hycient195 <onyeukwuhycient@gmail.com>
2024-11-13 22:48:59 +0100

    fix: page button spacing

--- src/app/app/finance-tracker/_components/SheetTablePage.tsx
+++ src/app/app/finance-tracker/_components/SheetTablePage.tsx
@@ -79,36 +79,44 @@ export default function BalanceSheetPage({ isLoggedIn, pages, setPages, canRedo,
           <div className={`${hasLogoOrSpinner ? "grid-cols-[90px_1fr_90px]" : "grid-cols-1"} table-top grid gap-3`}>
             {
               (isLoading.uploading || isLoading.deleting || isLoading.removingPage || page?.imageUrl)
-              ? <div className={`${isDragging ? "bg-green-500/40" : ""} relative !overflow-hidden z-[2] !w-[80px] !h-[80px] -translate-y-2 `} onDragOver={handleReceiptDragOver} onDragEnter={handleReceiptDragEnter} onDragLeave={handleReceiptDragLeave} onDrop={handleReceiptDrop}>
+              ? <div className={`${isDragging ? "bg-green-500/40" : ""} relative !overflow-hidde z-[2] !w-[80px] !h-[80px] -translate-y-2 `} onDragOver={handleReceiptDragOver} onDragEnter={handleReceiptDragEnter} onDragLeave={handleReceiptDragLeave} onDrop={handleReceiptDrop}>
                 {
                   (isLoading.uploading || isLoading.removingPage || isLoading.deleting)
                   ? (
                     <SaveLoadingSpinner height={70} width={70} className='' />
                   ) : (page.imageUrl && imageSrc) && (
-                    <Image fill src={imageSrc as string}  alt='alternative'  className='object-contain w-full h-auto aspect-square'  onDragOver={handleReceiptDragOver} onDragEnter={handleReceiptDragEnter} onDragLeave={handleReceiptDragLeave} onDrop={handleReceiptDrop} />
+                    <div className="relative w-full h-full ">
+                      <button onClick={() => handleRemoveLogo(page?.imageUrl?.split("<||>")[0] as string)} className="absolute noExport cursor-pointer z-[10] w-max h-max -top-3 -left-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="size-5 !text-zinc-400">
+                          <path strokeLinecap="round" strokeLinejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                        </svg>
+                      </button>
+                      <Image fill src={imageSrc as string}  alt='alternative'  className='object-contain w-full h-auto aspect-square'  onDragOver={handleReceiptDragOver} onDragEnter={handleReceiptDragEnter} onDragLeave={handleReceiptDragLeave} onDrop={handleReceiptDrop} />
+                    </div>
                   )
                 }
               </div>
               : (
-                <div className={`${isDragging ? "bg-green-500/20" : "bg-zinc-100"} absolute z-[1] h-[60px] w-[65px]  flex items-center p-2 justify-center text-center rounded  -top-3 -left-1 noExport`} onDragOver={handleReceiptDragOver} onDragEnter={handleReceiptDragEnter} onDragLeave={handleReceiptDragLeave} onDrop={handleReceiptDrop}>
-                  <span className="text-xs text-zinc-400">Drop Logo</span>
-                </div>
+                <label htmlFor={`add-logo-${pageIndex}`} className={`${isDragging ? "bg-green-500/20" : "bg-zinc-100"} absolute cursor-pointer z-[1] h-[70px] w-[75px]  flex items-center p-2 justify-center text-center rounded  -top-1  noExport`} onDragOver={handleReceiptDragOver} onDragEnter={handleReceiptDragEnter} onDragLeave={handleReceiptDragLeave} onDrop={handleReceiptDrop}>
+                  <span className="text-xs text-zinc-400">Add/drop Logo</span>
+                  <input id={`add-logo-${pageIndex}`} type="file" accept="image/jpeg, image/jpg, image/png" className='hidden' name={`${pageIndex}`} onChange={(e) => handleUploadLogo(e.target.files![0], pageIndex)} />
+                </label>
               )
             }
             <div className="titles grid !max-w-[800px] w-full mx-auto">
               <div className="relative h-max !max-w-[800px] w-full mx-auto bg-ts">
-                <p style={{ fontFamily: "sans-serif"}} className="invisib !max-w-[800px] w-full mx-auto py-1 text-2xl border- border-white outline-none font-bold w-ful text-center">{replaceJSXRecursive(page.title, { "\n": <br />})}<span className="invisible">.</span></p>
+                <p style={{ fontFamily: "sans-serif"}} className="invisib !max-w-[800px] text-black w-full mx-auto py-1 text-2xl border- border-white outline-none font-bold w-ful text-center">{replaceJSXRecursive(page.title, { "\n": <br />})}<span className="invisible">.</span></p>
                 <textarea style={{ fontFamily: "sans-serif" }} value={page.title} onChange={(e) => updatePageTitle(e.target.value, pageIndex)} placeholder='[ TITLE HERE... ]' autoFocus className="noExport !max-w-[800px] w-full mx-auto text-2xl py-1 resize-none absolute h-full !overflow-visible no-scrollbar top-0 left-0 right-0 outline-none border- border-zinc-300/80 font-bold w-ma w-ful text-center" />
               </div>
               <div className="mb-2 noExport" />
               <div className="relative mb-1">
-                <p style={{ fontFamily: "sans-serif"}} className="text-center  py-1 border- border-white invisibl outline-none text-lg text-black/80 font-semibold w-full">{page.subTitle}<span className="invisible">.</span></p>
+                <p style={{ fontFamily: "sans-serif"}} className="text-center  py-1 border- border-white text-black/90 invisibl outline-none text-lg text-black/80 font-semibold w-full">{page.subTitle}<span className="invisible">.</span></p>
                 <textarea style={{ fontFamily: "sans-serif" }} value={page.subTitle} onChange={(e) => updatePageSubtitle(e.target.value, pageIndex)} placeholder='[ SUBTITLE; eg. FROM PERIOD OF 1ST <MONTH> <YEAR> TO 30TH <MONTH> <YEAR>... ]' className={` noExport text-center py-1 resize-none absolute !overflow-visible no-scrollbar left-0 top-0 outline-none border- border-zinc-300/80 text-lg text-black/80 font-semibold h-full w-full`} />
               </div>
             </div>
           </div>
          
-          <div className="mb-3 noExport" />
+          <div className={`mb-3 ${!hasLogoOrSpinner ? "noExport" : ""}`} />
           <ResizableTable
             headers={["DATE", "NARRATION", "DEBIT", "CREDIT", "BALANCE"]}
             minCellWidth={100}
@@ -187,12 +195,12 @@ export default function BalanceSheetPage({ isLoggedIn, pages, setPages, canRedo,
             }
           />
           <div className="line noExport" />
-          <div className={`mt-6 noExport flex [&>*]:grow flex-wrap gap-3`}>
+          <div className={`mt-6 noExport flex [&>*]:grow flex-wrap gap-x-2.5 gap-y-2`}>
             <label htmlFor={`csv-import-${pageIndex}`} className="px-4 py-2 cursor-pointer text-center bg-rose-500 text-white rounded" >
               Load CSV
               <input id={`csv-import-${pageIndex}`} type="file" accept=".csv"  className='hidden' name={`${pageIndex}`} onChange={(e) => handleCSVImport(e, pageIndex)} />
             </label>
-            {
+            {/* {
               !imageSrc
               ? (
                 <label htmlFor={`add-logo-${pageIndex}`}  className="px-4 py-2 cursor-pointer text-center bg-violet-500 text-white rounded" >
@@ -204,7 +212,7 @@ export default function BalanceSheetPage({ isLoggedIn, pages, setPages, canRedo,
                   Remove Logo
                 </LoadingButton>
               )
-            }
+            } */}
            
             <button className="px-4 py-2 bg-amber-500 text-white rounded" onClick={() => downloadPageCSV(pageIndex)}>
               Download CSV
