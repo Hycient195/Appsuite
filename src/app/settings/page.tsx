24f750541f386a70f4e4b01b2f57e4aa1fc220ce
Hycient195 <onyeukwuhycient@gmail.com>
2024-12-03 17:32:21 +0100

    fix

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,7 +1,7 @@
 export default function Settings() {
   return (
     <main className="flex h-[100dvh] items-center justify-center p-4">
-      <h1 className="text-3xl lg:text-4xl font-medium">Coming soon</h1>
+      <h1 className="text-3xl lg:text-4xl font-medium">Coming s👀n</h1>
     </main>
   )
 }
