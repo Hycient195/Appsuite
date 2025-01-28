9f2e291786ffde89bdbb3ab20be5347a9ea190bf
Hycient195 <onyeukwuhycient@gmail.com>
2025-01-28 18:08:47 +0100

    fix

--- src/sharedComponents/ModuleFileHeader.tsx
+++ src/sharedComponents/ModuleFileHeader.tsx
@@ -50,7 +50,7 @@ export default function ModuleFileHeader({ moduleName, isSaving, isSavingError, 
     <section className="sticky -top-0 md:top- lg:-top-7 flex flex-col z-[3]">
       <div className="p-3.5 bg-primary lg:hidden flex flex-col gap-4">
         <div className="spread-out !gap-5 text-slate-100">
-          <div className="text-2xl text-slate-100 font-semibold line-in">
+          <div className="text-2xl text-slate-100 font-medium line-in">
             <LogoWhite className="!size-7" />
             {moduleName}
           </div>
@@ -65,7 +65,7 @@ export default function ModuleFileHeader({ moduleName, isSaving, isSavingError, 
         </div> */}
       </div>
       
-      <div className={`w-full  bg-slate-100 max-w-[1100px] mx-auto flex flex-col lg:grid lg:grid-cols-[1fr_max-content] border-b border-b-slate-300 gap-1 md:gap-x-3 max-md:text-center items-en flex-wrap pb-1.5 md:pb-2 pt-1 bg-tes px-2 lg:px-3 ${className}`}>
+      <div className={`w-full  bg-slate-100 max-w-[1100px] mx-auto flex flex-col lg:grid lg:grid-cols-[1fr_max-content] border-b border-b-slate-300 gap-1 md:gap-x-3 max-md:text-center items-en flex-wrap pb-1.5 md:pb-2 pt-1 bg-tes px-3 lg:px-3 ${className}`}>
         <div className="line-in lg:col-span-2 md:gap-4 text-sm max-lg:hidden">
           <HouseIcon />
           <ChevronRight className="!size-4" />
@@ -77,7 +77,7 @@ export default function ModuleFileHeader({ moduleName, isSaving, isSavingError, 
           
           <div className="grid b w-full overflow-x-auto">
             {/* <ResponsiveTextInput className="text-primary [&_*]:!w-max text-2xl font-semibold lg:mt-2 focus:ring-0 focus:outline-none" value={fileName} onChange={setFileName} /> */}
-            <input className="text-primary text-2xl bg-transparent font-semibold lg:mt-2 focus:ring-0 focus:outline-none" value={fileName} onChange={setFileName} />
+            <input placeholder="[ Document Title ]" className="text-primary text-2xl bg-transparent font-semibold lg:mt-2 focus:ring-0 focus:outline-none" value={fileName} onChange={setFileName} />
           </div>
           {/* <h1 className="text-primary text-2xl font-semibold mt-3 max-md:hidden">{fileName}</h1> */}
           <div className="line-in text-slate-700 lg:mt-2 !gap-4 text-sm">
@@ -94,7 +94,7 @@ export default function ModuleFileHeader({ moduleName, isSaving, isSavingError, 
           <div className="flex flex-col gap-1 relative flex-wrap items-center md:items-end">
             <button onClick={handleInitiateCreateFile} className="btn w-max bg-primary text-white max-lg:hidden"><PlusIcon className="!size-5 !stroke-[2px]" /> Create New Sheet</button>
             <p className="text-slate-400 text-sm">Last sync: <span className={`duration-500 ${isSavingSuccess ? "text-green-600" : isSavingError ? "text-red-500" : "text-slate-600"}`}>3rd December 2024, 15:00:00</span></p>
-            <LinearProgress color="inherit" className={`${!isSaving && "!hidden"} w-full text-primary !animate-fade-in !absolute -bottom-1 left-0`} />
+            <LinearProgress color="inherit" className={`${!isSaving && "!hidden"} w-full text-primary !animate-fade-in !absolute -bottom-0.5 left-0`} />
           </div>
         </div>
       </div>
